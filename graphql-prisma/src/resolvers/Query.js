a733e123947c9b619f50ad63886fc7a3c9b224bd
Iago Rocha Porto <iagorochaporto@gmail.com>
2020-02-18 20:41:48 -0300

    introduction for prisma

---
+++ graphql-prisma/src/resolvers/Query.js
@@ -0,0 +1,28 @@
+const Query = {
+  posts(parent, args, {db}, info) {
+    if(!args.query) {
+      return db.posts
+    }
+
+    return db.posts.filter((post) => {
+      const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+      const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+      
+      return isTitleMatch || isBodyMatch
+    })
+  },
+  users(parent, args, {db}, info) {
+    if(!args.query) {
+      return db.users
+    }
+
+    return db.users.filter((user) => {
+      return user.name.toLowerCase().includes(args.query.toLowerCase())
+    })
+  },
+  comments(parent, args, {db}, info) {
+    return db.comments
+  }
+}
+
+export default Query
